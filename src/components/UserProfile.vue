e1c70bb643126775c9fbb42d7563db70d623db2b
Tiago Carioca <tiagoacarioca@hotmail.com>
2020-09-23 10:46:53 -0400

    Chapter 5: Forms & Handling User Input

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -7,6 +7,21 @@
         <strong>Followers:</strong>
         {{ followers }}
       </div>
+      <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
+        <label for="newTwoot"><strong>New Twoot</strong></label>
+        <textarea id="newTwoot" rows="4" v-model="newTwootContent"></textarea>
+
+        <div class="user-profile__create-twoot-type">
+          <label for="newTwootType"><strong>Type: </strong></label>
+          <select id="newTwootType" v-model="selectedTwootType">
+            <option :value="option.value" v-for="(option, index) in twootTypes" :key="index">
+              {{ option.name }}
+            </option>
+          </select>
+        </div>
+
+        <button>Twoot!</button>
+      </form>
     </div>
     <div class="user-profile__twoots-wrapper">
         <TwootItem 
@@ -27,6 +42,12 @@ export default {
   components: { TwootItem },
   data() {
     return {
+      newTwootContent: '',
+      selectedTwootType: 'instant',
+      twootTypes: [
+        { value: 'draft', name: 'Draft' },
+        { value: 'instant', name: 'Instant Twoot' }
+      ],
       followers: 0,
       user: {
         id: 1,
@@ -60,6 +81,15 @@ export default {
     },
     toggleFavourite(id) {
         console.log(`Favourited Tweet #${id}`)
+    },
+    createNewTwoot() {
+      if (this.newTwootContent && this.selectedTwootType != 'draft') {
+        this.user.twoots.unshift({
+          id: this.user.twoots.length + 1,
+          content: this.newTwootContent
+        })
+        this.newTwootContent = '';
+      }
     }
   },
   mounted() {
@@ -94,5 +124,15 @@ h1 {
   margin-right: auto;
   padding: 0 10px;
   font-weight: bold;
+}
+
+.user-profile__create-twoot {
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+}
+
+.user-profile__create-twoot-type {
+  padding-top: 20px;
 }
 </style>
